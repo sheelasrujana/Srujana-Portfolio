fd0484ad464a103e8b3c7502641aaf4ff5d64f7b
sheelasrujana <sheelasrujana23@gmail.com>
2025-06-04 17:09:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+document.querySelector("#scroll-down").addEventListener("click", () => {
+    window.scrollTo({
+      top: document.querySelector("#about-me").offsetTop - 20,
+    });
+  });
+  
+  document.querySelector("#toggle-theme").addEventListener("click", () => {
+    document.documentElement.classList.toggle("light-theme");
+  });
+  
+  const prefersLight = window.matchMedia(
+    "(prefers-color-scheme: light)"
+  )?.matches;
+  if (prefersLight) {
+    document.documentElement.classList.toggle("light-theme");
+  }
+  
